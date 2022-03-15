701c587f043da20018a4b5bdd171047a48e98a8d
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-03-15 12:15:51 +0900

    namespace로 축약

--- 2022_CPP/00 C++기본.cpp
+++ 2022_CPP/00 C++기본.cpp
@@ -1,8 +1,11 @@
 //std::cout를 포함하고 있음
 #include <iostream>
+// std::를 생략 (안쓰는 사람도 많음!)
+using namespace std;
 
 int main(void) {
 	// std::cout 객체로 문자열을 출력
-	std::cout << "안녕 세상아\n";
+	// std::endl객체로 개항처리
+	std::cout << "안녕 세상아"<< std::endl;
 	return 0;
 }
