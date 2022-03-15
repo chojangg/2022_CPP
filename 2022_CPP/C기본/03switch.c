6a68d6f5501a81569e3b4a5e3154f653f6779bf7
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-03-15 12:17:59 +0900

    Add files via upload

---
+++ 2022_CPP/C기본/03switch.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int x = 1;
+
+	switch (x) {
+	case 0:
+		printf("0");
+	case 1:
+		printf("1");
+		// break가 없기 때문에 콘솔창에는 "01"출력
+	case 2:
+		printf("2");
+		break;
+	default:
+		printf("d");
+		break;
+	}
+	return 0;
+}
