46dbef441380f2f0f65b57195264b4282bb622b4
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-03-18 10:20:59 +0900

    구조체 값이 바뀜(Call By Value)

--- 2022_CPP/12 구조체 포인터.c
+++ 2022_CPP/12 구조체 포인터.c
@@ -9,16 +9,17 @@ typedef struct {
 void AddValueToPoint(Point p);
 
 int main(void) {
-	Point point;
+	Point point;	// stack 할당
 	point.x = 5.0f;
 	point.y = 10.0f;
-	AddValueToPoint(point);
+	// call by pointer라서 값이 바뀜
+	AddValueToPoint(point); //argument
 	printf("%1.f, %1.f\n", point.x, point.y);
 
 	return 0;
 }
 
 void AddValueToPoint(Point p) {
-	p.x += 10.0f;
-	p.y += 15.0f;
+	p->x += 10.0f;
+	p->y += 15.0f;
 }
