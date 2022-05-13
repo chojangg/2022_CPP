0e8b47ec789f6defda96272f6a21b18266b8bbee
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-05-13 10:19:09 +0900

    객체배열 동적할당

--- 2022_CPP/03-01 class 생성자.cpp
+++ 2022_CPP/03-01 class 생성자.cpp
@@ -1,5 +1,6 @@
 #include <iostream>	// cout, endl 사용
 #include <string>
+
 using namespace std;
 
 class Student
@@ -13,6 +14,7 @@ public:
 	// 반환형을 갖고있지 않는다
 	Student();
 	Student(int Hakbun, string Name);
+
 	void show();
 };
 
@@ -20,18 +22,15 @@ Student::Student()
 {
 	this->nHakbun = 1234;
 	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 생성." << endl;
 }
 
 // 멤버변수를 초기화 할 수 있으며 따라서,
 // const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
-
 Student::Student(int Hakbun, string Name)
 	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
 {
-	this->nHakbun = Hakbun;
-	this->sName = Name;
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 생성." << endl;
 }
 
 void Student::show()
@@ -42,11 +41,11 @@ void Student::show()
 
 int main(void)
 {
-	Student* stu2 = new Student(1111, "JWP");
-	stu2->show();
+	Student* stu2 = new Student[6];
+	for (int i = 0; i < 6; i++)
+		stu2[i].show();
 
-	delete stu2;
+	delete []stu2;
 
 	return 0;
-
 }
