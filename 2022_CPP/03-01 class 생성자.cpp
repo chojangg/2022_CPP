cd3148def4ef8f79021fff354640da93f7b3f154
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-05-13 09:56:57 +0900

    배열 동적할당

--- 2022_CPP/03-01 class 생성자.cpp
+++ 2022_CPP/03-01 class 생성자.cpp
@@ -25,6 +25,7 @@ Student::Student()
 
 // 멤버변수를 초기화 할 수 있으며 따라서,
 // const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
+
 Student::Student(int Hakbun, string Name)
 	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
 {
@@ -47,13 +48,14 @@ int main(void)
 	//Student stu2 = Student(1111, "JWP");
 	//stu2.show();
 
-	int* ptr1 = new int;
-	*ptr1 = 20;
-	cout << *ptr1 << endl;
+	int* ptr2 = new int[4];
+	for (int i = 0; i < 4; i++) {
+		ptr2[i] = 10 + i;
+		cout << ptr2[i] << " ";
+	}
 
-	delete ptr1;
+	delete[]ptr2;
 
 	return 0;
-
 
 }
