c08709223e25140a0fee56e2bace07c72cb7d8ae
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-06-03 09:37:20 +0900

    매개변수(mX, mY), 생성자

---
+++ 2022_CPP/04-01 연산자 오버로딩(기본).cpp
@@ -0,0 +1,25 @@
+#include <iostream>	// cout, endl 사용
+#include <string.h>	// strcpy() 사용
+
+using namespace std;
+
+class Point {
+public:
+	Point(int m_X, int m_Y);
+private:
+	int mX;
+	int mY;
+	
+};
+
+Point::Point(int m_X, int m_Y)
+{
+	mX = m_X;
+	mY = m_Y;
+}
+
+int main(void)
+{
+	Point p1(1, 2);
+	return 0;
+}
