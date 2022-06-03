470caf326365beec50211b18cf1c591b0e61de8b
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-06-03 09:46:52 +0900

    출력

--- 2022_CPP/04-01 연산자 오버로딩(기본).cpp
+++ 2022_CPP/04-01 연산자 오버로딩(기본).cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Point {
 public:
 	Point(int m_X, int m_Y);
+	void print(void);
 private:
 	int mX;
 	int mY;
@@ -14,8 +15,13 @@ private:
 
 Point::Point(int _x, int _y) : mX(_x), mY(_y) {}
 
+void Point::print(void) {
+	cout << "(" << mX << ", " << mY << ")" << endl;
+}
+
 int main(void)
 {
 	Point p1(1, 2);
+	p1.print();
 	return 0;
 }
