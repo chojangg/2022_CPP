b5a24346de90afa0aa952d1ce2e3f2369b41fc52
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-06-17 09:18:22 +0900

    상속 기본예제

--- 2022_CPP/04-03 대입연산자 오버로딩.cpp
+++ 2022_CPP/04-03 대입연산자 오버로딩.cpp
@@ -1,5 +1,6 @@
 #include <iostream>	// cout, endl 사용
 #include <string.h>	// strcpy() 사용
+
 using namespace std;
 
 class Student
@@ -22,13 +23,13 @@ public:
 Student::Student()
 {
 }
+
 // 멤버변수를 초기화 할 수 있으며 따라서,
 // const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
-
 Student::Student(int Hakbun, const char* Name)
 	: nHakbun(Hakbun)	// 멤버변수(매개변수)
 {
-	cout << "일반생성자 호출." << endl;
+	cout << "Student일반생성자 호출." << endl;
 	int len = strlen(Name) + 1;		// 공간의 갯수 파악
 	sName = new char[len];			// 갯수만큼 메모리 할당
 	strcpy(sName, Name);
@@ -38,7 +39,7 @@ Student::Student(int Hakbun, const char* Name)
 Student::Student(const Student& rhs)
 	:nHakbun(rhs.nHakbun), sName(rhs.sName)
 {
-	cout << "복사생성자 호출" << endl;
+	cout << "Student 복사생성자 호출" << endl;
 	int len = strlen(rhs.sName) + 1;		// 공간의 갯수 파악
 	sName = new char[len];			// 갯수만큼 메모리 할당
 	strcpy(sName, rhs.sName);
@@ -69,17 +70,27 @@ Student& Student::operator=(const Student& rhs)
 	return *this;
 }
 
+class HighSchoolStudent : public Student
+{
+public:
+	HighSchoolStudent(int Hakbun, const char* Name, bool isD)
+		: Student(Hakbun, Name), isDormitory(isD)
+	{
+		cout << "HighSchoolStudent 일반생성자 호출" << endl;
+	}
+
+	~HighSchoolStudent()
+	{
+		cout << "HighSchoolStudent 소명자 호출" << endl;
+	}
+private:
+	bool isDormitory;
+};
+
 int main(void)
 {
-	// "일반생성자 호출" 출력
-	Student stu1 = Student(1111, "JWP");
-	Student stu3 = Student(2222, "JYP");
-	stu1.show();		// (1111, "JWP")
-	// 복사생성자 호출
-	Student stu2 = stu1;	// stu2 = Student(stu1)
-	stu2.show();		// (1111, "JWP")
-	// 대입연산자 호출 (아직 오버로딩 구현 안함)
-	stu1 = stu3;
-	stu1.show();		// (2222, "JYP")
+	HighSchoolStudent hss = HighSchoolStudent(1111, "JWP", false);
+	hss.show();
+
 	return 0;
 }
