3293f02a4eecf754343be4f257366dad5945ca8d
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-08-30 12:12:49 +0900

    순수가상함수 (추상화)

---
+++ 2022_CPP/06-03 순수가상함수.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+
+public:
+	virtual void eat(void) = 0;
+	virtual void roar(void) = 0;
+	virtual void walk(void) = 0;
+
+private:
+	string name;
+	int sex;
+	int age;
+};
+
+class Tiger : public Animal {
+public:
+	void eat(void) override { cout << "È£¶ûÀÌ ³È³È" << endl; }
+	void roar(void) override { cout << "¾îÈï" << endl; }
+	void walk(void) override { cout << "È£¶ûÀÌ ´ç´ç" << endl; }
+};
+
+class Dog : public Animal {
+public:
+	void eat(void) override { cout << "°³ ÂÁÂÁ" << endl; }
+	void roar(void) override { cout << "¸Û¸Û" << endl; }
+	void walk(void) override { cout << "°³ »ç»Ó" << endl; }
+};
+
+void main(void) {
+	Animal* animal = new Tiger();
+	delete animal;
+}
