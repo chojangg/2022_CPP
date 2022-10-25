8ab7b5de02d1a8d471d005597a174c47b57fa9d4
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-10-25 11:13:28 +0900

    오류 수정

--- 2022_CPP/08-02 map.cpp
+++ 2022_CPP/08-02 map.cpp
@@ -9,18 +9,18 @@ using namespace std;
 // 권민수: 주종혁
 // 이준호: 강태오
 
-int main(void)
+void main(void)
 {
-	map<string, string> IBW;	// IBW: 이상한 변호사 우영우
-	IBW.insert(pair<string, string>("우영우", "박은빈"));
-	IBW.insert(pair<string, string>("정명석", "강기영"));
-	IBW.insert(pair<string, string>("권민우", "주종혁"));
-	IBW.insert(pair<string, string>("이준호", "강태오"));
+    map<string, string> IBW;   //IBW : 이상한 변호사 우영우
+    IBW["우영우"] = "박은빈";
+    IBW["정명석"] = "강기영";
+    IBW["권민우"] = "주종혁";
+    IBW["이준호"] = "강태오";
 
-	// vector의 모든 원소 출력(반복자)
-	for (auto& iter = IBW.begin(); iter != IBW.end(); iter++)
-		cout << iter->first << "역 " << iter->second << "분" << endl;
 
-	cout << "드라마의 주인공은 " << IBW["우영우"]<< endl;;
+    for (auto& iter : IBW) {
+        cout << iter.first << "역 " << iter.second << "분" << endl;
+    }
 
+    cout << "드라마의 주인공은 " << IBW["우영우"] << endl;
 }
